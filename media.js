71b7d0b943206f8b694e4665010816eb90f15d24
Abdushukur <abduhakimovabdushukur@gmail.com>
2022-10-05 13:47:36 +0500

    jc

---
+++ media.js
@@ -0,0 +1,38 @@
+// let user = prompt('son kiriting')
+
+
+// if ( isNaN(user)) {
+//     alert(' bu son emas , iltimos son  kiriting')
+// }else{
+//     alert(' bu son emas , iltimos son  kiriting')
+// }
+//  let usernumber = +prompt('son kiriting')
+//  a = 0
+// if ( usernumber / 2 == resolt && 7 + 7 ==6 ) {
+//     alert(resolt)
+// }else{
+//     alert('bu toq son')
+// }
+// let answer = +prompt('son kiriting')
+// a = 0
+// if(  answer / 2 == a){
+//     alert(' bu juft son')
+// }else{
+//     alert('bu toq son')
+// }
+let number1 = +prompt('birinchi son kiriting')
+let number2 = +prompt('ikkinchi soni kiriting')
+let number3 = +prompt('uchinchi soni kiriting')
+a = number1
+b = number2
+c = number3
+if (a < b < c) {
+    alert(number2 + " o'rta qiymat shu ")
+}else if ( b  < a < c ) {
+    alert( number1 + " o'rta qiymat shu ")
+}else if (b < C < a) {
+    alert( number3 +" o'rta qiymat shu ")
+}else {
+    alert('nimadur xato')
+}
+    
